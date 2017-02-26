3bc15a7626cfcb33ba36cac1fff4ccd2c02af53e
jihaichuan <jihaichuan@126.com>
2017-02-26 22:32:54 +0800

    更新内容： 1. 新增form 表单的用法 2. 修改 index.html 新增 form 的链接地址

---
+++ js/form.js
@@ -0,0 +1,11 @@
+$('.ui.fluid.dropdown').dropdown();
+
+$('.ui.fluid.search.section.dropdown').dropdown();
+
+$(".ui.search.dropdown").dropdown();
+
+// 卡类型
+$(".ui.selection.dropdown").dropdown();
+
+/** 多选框 checkbox **/
+$(".ui.checkbox").checkbox();
